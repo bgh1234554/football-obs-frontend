0132434ba007ef855648e26f8c63d4f200db01a0
Indvel <fbtkdqja112@gmail.com>
2026-06-26 18:20:58 +0900

    테마 관련 추가 작업

--- js/core/fsm-board.js
+++ js/core/fsm-board.js
@@ -32,9 +32,9 @@ function applyTheme(theme, logoUrl) {
   // FSM의 switch(data.theme) 블록을 함수로 추출한 것
   switch(theme) {
     case 'pl':
-      changeCSS('./styles/result_style_PL.css', CSS_LINK_INDEX);
+      changeCSS('../css/core/theme/board-theme-epl.css', CSS_LINK_INDEX);
       break;
     default:
-      changeCSS('./styles/result_style.css', CSS_LINK_INDEX);
+      changeCSS('../css/core/theme/board-theme-default.css', CSS_LINK_INDEX);
   }
 }
