e980a5221ff71bd30804a42ca7e0ac9e01e5aa67
Indvel <fbtkdqja112@gmail.com>
2026-09-02 18:22:26 +0900

    Trying to fix Refresh Error

--- js/core/fsm-board.js
+++ js/core/fsm-board.js
@@ -28,6 +28,8 @@ window.autoApplyTemplateByLeagueId = function(leagueId, apiLeagueLogoUrl) {
 
   _currentTheme = theme;
   _currentType = type;
+  state.leagueId = leagueId;
+  state.leagueLogoUrl = logoUrl;
   // FSM의 기존 switch 로직을 그대로 재활용
   applyTheme(theme, logoUrl);
 };
